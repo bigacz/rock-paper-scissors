272a9883cde72556b80082e7132b5ab58f7bdc4c
bigacz <sbigacz@gmail.com>
2023-05-24 10:18:26 +0200

    Add game with up to 10 rounds, fix minor issues

--- app.js
+++ app.js
@@ -7,63 +7,82 @@ function getComputerChoice() {
 
 function playRound() {
     let computerChoice = getComputerChoice();
-    let playerChoice = prompt().toLowerCase();
+    let playerChoice = prompt("Your move")?.toLowerCase();
     switch(playerChoice) {
         case "rock":
-            moveRock(computerChoice);
-            break;    
+            return moveRock(computerChoice);
         case "paper":
-            movePaper(computerChoice);
-            break;
+            return movePaper(computerChoice);
         case "scissors":
-            moveScissors(computerChoice);
-            break;
+            return moveScissors(computerChoice);
         default:
             console.log("Wrong input!");
-            break;
+            playRound();
     }
 }
 
 function moveRock(computerChoice) {
+    let result;
     switch(computerChoice) {
         case "rock": 
-            console.log("Draw");
-            break;
+            return 0;
         case "paper":
-            console.log("Lose");
-            break;
+            return -1;
         case "scissors":
-            console.log("Win");
-            break;
-    }
+            return 1
+
+    } 
 }
 
 function movePaper(computerChoice) {
     switch(computerChoice) {
         case "rock": 
-            console.log("Win");
-            break;
+            return 1
         case "paper":
-            console.log("Draw");
-            break;
+            return 0;
         case "scissors":
-            console.log("Lose");
-            break;
-    }       
+            return -1;
+    }        
 }
 
 function moveScissors(computerChoice) {
     switch(computerChoice) {
         case "rock": 
-            console.log("Lose");
-            break;
+            return -1;
         case "paper":
-            console.log("Win");
-            break;
+            return 1
         case "scissors":
-            console.log("Draw");
-            break;
-    }   
+            return 0;
+    }  
 }
 
-playRound();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    while(playerScore < 5 && computerScore < 5)
+    {
+        let result = playRound();
+        switch(result) {
+            case 1:
+                playerScore++;
+                console.log("Win");
+                break;
+            case -1:  
+                computerScore++;
+                console.log("Lose");
+                break;
+            default:
+                console.log("Draw");
+                break;            
+        }
+        console.log(`You: ${playerScore} Computer: ${computerScore}`)
+    }
+    if(playerScore >= 5) {
+        console.log("You WON the game!")
+    }
+    else {
+        console.log("You LOST the game!")
+    }
+}
+
+game();
